5d523d4664462834c55f3b35aac45953dd7e3f20
Franco B. <franco.barravecchia2002@gmail.com>
2024-09-04 15:32:21 -0300

    update: specific material3 version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation("libs.androidx.material3:material3::1.2.0-rc01")
     implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
