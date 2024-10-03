90d1fdfa64d24ef667a12c9474f8f30f5a7f3503
Franco B. <franco.barravecchia2002@gmail.com>
2024-10-02 23:53:24 -0300

    update: manage no favourites

--- app/src/main/java/com/recipesbook/favourites/FavouritesComposable.kt
+++ app/src/main/java/com/recipesbook/favourites/FavouritesComposable.kt
@@ -1,15 +1,19 @@
 package com.recipesbook.favourites
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,21 +35,29 @@ fun Favourite() {
         TODO("show retry icon")
     } else {
         Log.d("DEBUG", "FINISHED LOADING")
-        //TODO: add empty list management
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(2),
-            modifier = Modifier.fillMaxHeight()
-        ) {
-            items(favourites) { recipe ->
-                RecipeCard(
-                    RecipeModel(recipe.idMeal, recipe.name, recipe.imageUrl),
-                    Modifier
-                        .fillMaxHeight(0.5f)
-                        .fillMaxWidth()
-                        .padding(8.dp)
+        //TODO: improve empty list management
+        if (favourites.size == 0)
+            Box(modifier = Modifier.fillMaxSize()) {
+                Text(
+                    text = "You don't have favourite recipes",
+                    modifier = Modifier.align(Alignment.Center)
                 )
             }
-        }
+        else
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.fillMaxHeight()
+            ) {
+                items(favourites) { recipe ->
+                    RecipeCard(
+                        RecipeModel(recipe.idMeal, recipe.name, recipe.imageUrl),
+                        Modifier
+                            .fillMaxHeight(0.5f)
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                }
+            }
     }
 }
 
