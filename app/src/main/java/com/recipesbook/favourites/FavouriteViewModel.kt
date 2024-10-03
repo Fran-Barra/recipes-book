bbfe9e632379c5683cb9780ba299bd0719669749
Franco B. <franco.barravecchia2002@gmail.com>
2024-10-02 23:31:59 -0300

    update: Favourite composable and view model

--- app/src/main/java/com/recipesbook/favourites/FavouriteViewModel.kt
+++ app/src/main/java/com/recipesbook/favourites/FavouriteViewModel.kt
@@ -1,20 +1,34 @@
 package com.recipesbook.favourites
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.recipesbook.RecipeModel
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.viewModelScope
+import com.recipesbook.DetailedRecipeModel
 import com.recipesbook.apiManagement.RecipeBookApiImpl
-import com.recipesbook.apiManagement.RecipesBookApi
+import com.recipesbook.apiManagement.RequestResponseCallbacks
+import com.recipesbook.data.RecipesBookDatabase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class FavouriteViewModel @Inject constructor(private val api : RecipeBookApiImpl) : ViewModel() {
+class FavouriteViewModel @Inject constructor(
+    @ApplicationContext val context: Context,
+    private val api : RecipeBookApiImpl
+) : ViewModel() {
+    private val recipesBookDatabase = RecipesBookDatabase.getDatabase(context)
+    private val favouriteRecipesIds = recipesBookDatabase.favouriteDao().getAllFavourites().asFlow()
+
+
     private val _loadingFavourites = MutableStateFlow(false)
     val loadingFavourite = _loadingFavourites.asStateFlow()
 
-    private val _favourites = MutableStateFlow(listOf<RecipeModel>())
+    private val _favourites = MutableStateFlow(listOf<DetailedRecipeModel>())
     val favourites = _favourites.asStateFlow()
 
     private val _showRetry = MutableStateFlow(false)
@@ -25,8 +39,50 @@ class FavouriteViewModel @Inject constructor(private val api : RecipeBookApiImpl
     fun retryLoadFavourites() = loadFavourites()
 
 
+    //TODO: make it async -> what happens if too many ids?
     private fun loadFavourites() {
-        _loadingFavourites.value = true
-        TODO("this should firs pick from te database and then from the api")
+        viewModelScope.launch {
+            _loadingFavourites.value = true
+
+            favouriteRecipesIds.collect { favouritesList ->
+                if (favouritesList.isNotEmpty()) {
+                    Log.d("DEBUG", "FAVOURITES FOUND")
+                    val ids = favouritesList.map { it.idMeal }
+                    fetchRecipesFromApi(ids)
+                } else {
+                    //TODO: used for testing, remove when add integrated
+                    recipesBookDatabase.favouriteDao().
+                        insert(com.recipesbook.data.Favourite(idMeal = "52772"))
+                    _loadingFavourites.value = false
+                }
+            }
+        }
+    }
+
+    private fun fetchRecipesFromApi(ids : List<String>) {
+        var remainingRequests = ids.size
+
+        for (recipeId in ids) {
+            api.getDetailedModel(recipeId, RequestResponseCallbacks(
+                context,
+                onSuccess = {
+                    viewModelScope.launch {
+                        _favourites.emit((_favourites.value+it.meals.first()).sortedBy { it.idMeal })
+                    } },
+                onFail = { _showRetry.value = true },
+                loadingFinished = {
+                    remainingRequests--
+                    if (remainingRequests<=0) _loadingFavourites.value = false
+                }
+            ))
+        }
+    }
+
+    fun removeFavourite(idMeal : String) {
+        viewModelScope.launch {
+            recipesBookDatabase.favouriteDao().delete(com.recipesbook.data.Favourite(idMeal = idMeal))
+            val model = _favourites.value.findLast { r -> r.idMeal == idMeal }
+            if (model != null) _favourites.value-=model
+        }
     }
 }
--- app/src/main/java/com/recipesbook/favourites/FavouritesComposable.kt
+++ app/src/main/java/com/recipesbook/favourites/FavouritesComposable.kt
@@ -1,14 +1,49 @@
 package com.recipesbook.favourites
 
-import androidx.compose.material3.Text
+import android.util.Log
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.recipesbook.RecipeModel
+import com.recipesbook.composable.common.RecipeCard
 
 @Composable
 fun Favourite() {
     val viewModel = hiltViewModel<FavouriteViewModel>()
-    Text(text = "We are building the favourites page here")
+
+    val favourites by viewModel.favourites.collectAsState();
+    val loadingFavourites by viewModel.loadingFavourite.collectAsState();
+    val showRetry by viewModel.showRetry.collectAsState();
+
+    if (loadingFavourites) {
+        //TODO("loading randoms icon")
+    } else if (showRetry) {
+        TODO("show retry icon")
+    } else {
+        Log.d("DEBUG", "FINISHED LOADING")
+        //TODO: add empty list management
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxHeight()
+        ) {
+            items(favourites) { recipe ->
+                RecipeCard(
+                    RecipeModel(recipe.idMeal, recipe.name, recipe.imageUrl),
+                    Modifier.fillMaxHeight(0.5f).fillMaxWidth().padding(8.dp)
+                )
+            }
+        }
+    }
 }
 
 @Preview
