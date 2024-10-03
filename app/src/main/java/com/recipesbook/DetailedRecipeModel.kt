be15d3d0427c9d9da919084a5e035f5b2ccd7120
Franco B. <franco.barravecchia2002@gmail.com>
2024-10-02 23:31:36 -0300

    update: DetailedRecipeModel with new data

--- app/src/main/java/com/recipesbook/DetailedRecipeModel.kt
+++ app/src/main/java/com/recipesbook/DetailedRecipeModel.kt
@@ -1,6 +1,10 @@
 package com.recipesbook
 
+import com.google.gson.annotations.SerializedName
+
 data class DetailedRecipeModel(
-    val id : String,
-    val strInstructions : String
+    val idMeal: String,
+    @SerializedName("strMeal") val name : String,
+    @SerializedName("strMealThumb") val imageUrl : String,
+    @SerializedName("strInstructions") val instructions : String
 )
