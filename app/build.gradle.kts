0b81dd1a487efb7e9113066a087c66244ccd45cb
Franco B. <franco.barravecchia2002@gmail.com>
2024-10-23 15:09:14 -0300

    update: add biometic in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,11 +71,13 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation("com.google.dagger:hilt-android:2.49")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.49")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit:converter-gson:2.0.0-beta2")
     implementation("io.coil-kt:coil-compose:2.2.2")
+
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
 
     //database
     implementation("androidx.room:room-runtime:2.6.1")
