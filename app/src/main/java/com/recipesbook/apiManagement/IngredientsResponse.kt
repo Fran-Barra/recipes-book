ddf5a56134b0beb7ca56ff13ddd533024d5d98d2
Franco B. <franco.barravecchia2002@gmail.com>
2024-11-18 19:37:09 -0300

    fix: IngredientsResponse

--- app/src/main/java/com/recipesbook/apiManagement/IngredientsResponse.kt
+++ app/src/main/java/com/recipesbook/apiManagement/IngredientsResponse.kt
@@ -2,7 +2,7 @@ package com.recipesbook.apiManagement
 
 import com.google.gson.annotations.SerializedName
 
-data class IngredientsResponse(val ingredient : List<IngredientResponse>)
+data class IngredientsResponse(@SerializedName("meals") val ingredient : List<IngredientResponse>)
 
 data class IngredientResponse(
     @SerializedName("idIngredient") val id : String,
