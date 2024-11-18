678d8abab95a20e8176e22abc0de73f470a65331
Franco B. <franco.barravecchia2002@gmail.com>
2024-11-18 19:46:51 -0300

    fix: Retry display

--- app/src/main/java/com/recipesbook/composable/common/Retry.kt
+++ app/src/main/java/com/recipesbook/composable/common/Retry.kt
@@ -1,7 +1,11 @@
 package com.recipesbook.composable.common
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,21 +13,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.recipesbook.R
+import com.recipesbook.ui.theme.Dimensions
 
 @Composable
 fun Retry(
     onClickRetry : () -> Unit,
     text : String = stringResource(id = R.string.something_went_wrong)
 ) {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(text = text, modifier = Modifier.align(Alignment.Center))
-        Button(
-            onClick = onClickRetry,
-            modifier = Modifier.align(Alignment.Center)
-        ) {
-            Text(
-                text = stringResource(R.string.retry),
-            )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = text)
+        Spacer(modifier = Modifier.height(Dimensions.SpaceBy.medium))
+        Button(onClick = onClickRetry) {
+            Text(text = stringResource(R.string.retry))
         }
     }
+
 }
